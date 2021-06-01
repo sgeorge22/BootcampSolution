6866ce4549156978ba3bf6434078a8e48012d786
Sarah George <sarahgeorge1394@gmail.com>
2021-06-01 12:29:21 -0400

    Add project files.

---
+++ Bootcamp/Models/BootcampContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bootcamp.Models
+{
+    public class BootcampContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) // one time sets up the connection between sqlserver - slide311
+        {
+            if (!builder.IsConfigured)
+            {
+                var connStr = "server=localhost\\sqlexpress01;database=BootcampDb;trusted_connection=true;";
+                builder.UseSqlServer(connStr); //runs the first time anything in our code uses the DbContext
+
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder) //where you put the fluent_abi - slide312
+        {
+
+        }
+    }
+}
---
+++ Bootcamp/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Bootcamp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Firstname { get; set; }
+        [Required, StringLength(50)]
+        public string Lastname { get; set; }
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal TargetSalary { get; set; }
+        public bool? InBootcamp { get; set; }
+
+
+        public Student() //Default constructor, not really needed anymore but okay to have 
+        { 
+
+        }
+    }
+}
